7243b05f0e02ee9d1bdf8603f428ed3c32a1dc5d
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-08-22 19:55:36 +0000

    Modified HST gentools gen_contexts.py to generate .jmap's which record infrequently changing metadata which is derived from CDBS config files and parallels each .imap.  The long term intent is to make instrument level reference file parameter substitution rules and .tpn associations first class versioned data,  kind of an instrument schema.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/hst_gentools@1097 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- gen_contexts.py
+++ gen_contexts.py
@@ -2,12 +2,13 @@ import sys
 import pprint
 import os.path
 
+from crds.hst import substitutions, tpn
 from crds import (rmap, log, timestamp)
 from crds.compat import OrderedDict
 
 import crds.hst.parkeys as parkeys
 
-def generate_imap(instr, filekinds):
+def generate_imap(instr, filekinds, jmap):
     now = str(timestamp.now())
     name = "hst_" + instr + ".imap"
     header = OrderedDict([
@@ -17,12 +18,12 @@ def generate_imap(instr, filekinds):
       ("observatory" , "HST"),
       ("instrument", instr.upper()),
       ('parkey', ('REFTYPE',)),
+      ('metadata', jmap),
       # ("description", ("Initially generated on " + now)),
     ])
 
     imap = "../CRDS/hst/mappings/hst/hst_"  + instr + ".imap"
     log.info("writing", imap)
-
     selector = {}
     for keyword in filekinds:
         eventually_generated_rmap = "hst_" + instr + "_" + keyword + ".rmap"
@@ -30,13 +31,39 @@ def generate_imap(instr, filekinds):
             selector[keyword] = eventually_generated_rmap
         else:
             log.warning("Skipping non-existent", eventually_generated_rmap)
-
     imap = rmap.Mapping(imap, header, selector)
     imap.write()
 
+def generate_jmap(instr):
+    """The .jmap contains infrequently changing metadata which is used to
+    associate references with rmaps and update rmaps.
+    """
+    now = str(timestamp.now())
+    path = "../CRDS/hst/mappings/hst/hst_"  + instr + ".jmap"
+    name = os.path.basename(path)
+    typenames, parkeys, selector = tpn.get_filekind_metadata(instr)
+    header = OrderedDict([
+      ("name", name),
+      ("derived_from", "generated " + now),
+      ("mapping", "META"),
+      ("observatory" , "HST"),
+      ("instrument", instr.upper()),
+      ('parkey', parkeys),
+      ('typenames', typenames),
+      # ('fi_equivalents', tpn.FILETYPE_TO_SUFFIX[instr]),
+      # ('filekind_to_suffix', tpn.FILEKIND_TO_SUFFIX[instr]),
+      ('expansion_rules', substitutions.get_substitutions(instr)),
+      # ("description", ("Initially generated on " + now)),
+    ])
+    log.info("writing", path)
+    jmap = rmap.Mapping(path, header, selector)
+    jmap.write()
+    return name
+
 def main():
     for instr in parkeys.get_instruments():
-        generate_imap(instr, parkeys.get_filekinds(instr))
+        jmap = generate_jmap(instr)
+        generate_imap(instr, parkeys.get_filekinds(instr), jmap)
     log.standard_status()
 
 if __name__ == "__main__":
