1248434d69c494454ceeb367753d392dbc1c2fdd
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-10 17:19:55 +0000

    Modified hst_gentools gen_acs_biasfile_ids.py to work on all instruments and emit counts of represented datasets for each representative dataset.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/hst_gentools@1422 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- gen_acs_biasfile_ids.py
+++ gen_acs_biasfile_ids.py
@@ -8,21 +8,29 @@ from crds import log, timestamp, utils, rmap
 
 SM4 = timestamp.reformat_date("2009-05-14 00:00:00.000000")
 
-def main(context):   
-    headers = api.get_dataset_headers_by_instrument(context, "acs")
-    params = rmap.get_cached_mapping(context).get_imap("acs").get_required_parkeys()
+def main(context): 
+    """Determine ids for unique cases,  discriminating between pre and post SM4 but otherwise
+    removing DATE-OBS and TIME-OBS.   Keeping both pre and post SM4 cases exposes different
+    special case matching hook branches.   Matching parameters still determined independently
+    by CDBS OPUS pipeline bestrefs (CDBS) and DADSOPS (CRDS).
+    """
+    pmap = rmap.get_cached_mapping(context)
     unique = defaultdict(list)
-    for (dataset_id, header) in headers.items():
-        unique[get_unique_key(header, params)].append(dataset_id)
+    for instr in pmap.selections:
+        headers = api.get_dataset_headers_by_instrument(context, instr)
+        params = pmap.get_imap(instr).get_required_parkeys()
+        for (dataset_id, header) in headers.items():
+            unique[get_unique_key(header, params)].append(dataset_id)
     for key, val in unique.items():
         print(val[-1])
-        print(val[-1], len(val), file=sys.stderr)
+        log.info(val[-1], len(val))
+    log.standard_status()
     
 def get_unique_key(header, params):
     header = dict(header)
-    EXPSTART = timestamp.reformat_date(header.pop("DATE-OBS") + " " + header.pop("TIME-OBS"))
+    expstart = timestamp.reformat_date(header.pop("DATE-OBS") + " " + header.pop("TIME-OBS"))
     key = tuple([(key, utils.condition_value(val)) for (key, val) in header.items() if key in params])
-    key = key + (("SM4", EXPSTART <= SM4,),)
+    key = key + (("SM4", expstart  <= SM4,),)
     return key
 
 if __name__ == "__main__":
