398306bcde6c3f45632fc163734a887e1ef3c72d
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-03-06 21:01:07 +0000

    Updated HST milestone-5 with .tpn and .rule updates supported recent ACS and STIS changes, particularly for ACS BIASFILE APERTURE=ANY_FULL includes NONE,  permitting the creation of a new rmap for y2k1610hj_bia.fits which also matches on NONE and still certifies.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/milestone-5@1453 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- lib/svn_version.py
+++ lib/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1242M'
+__svn_version__ = '1452M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://subversion.assembla.com/svn/crds/trunk
+URL: https://subversion.assembla.com/svn/crds/branches/milestone-5
 Repository Root: https://subversion.assembla.com/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1242
+Revision: 1452
 Node Kind: directory
 Schedule: normal
 Last Changed Author: stsci_jmiller
-Last Changed Rev: 1242
-Last Changed Date: 2013-11-22 15:07:16 -0500 (Fri, 22 Nov 2013)
+Last Changed Rev: 1261
+Last Changed Date: 2013-12-10 11:46:47 -0500 (Tue, 10 Dec 2013)
 
 '''
