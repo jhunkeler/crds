9c7ef3d968399a7ce543987d827564cb09817c7e
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-07-11 16:13:39 +0000

    Updated unit tests on milestone-7-b so they all pass minus NotImplemented
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/milestone-7-b@1657 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1655M'
+__svn_version__ = '1656M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS_fixed
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/milestone-7-b
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1655
+Revision: 1656
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1655
-Last Changed Date: 2014-07-11 11:29:31 -0400 (Fri, 11 Jul 2014)
+Last Changed Rev: 1656
+Last Changed Date: 2014-07-11 11:40:50 -0400 (Fri, 11 Jul 2014)
 
 '''
