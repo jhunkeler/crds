a6dbf25589156b748efd8ddb3a3fb0e983b5e91f
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-04 21:19:43 +0000

    Updated milestone-7-p from trunk,  everything except .tpn updates for multi-extension table checks.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/milestone-7-p@1772 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '1752M'
+__svn_version__ = '1760:1769M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1752
+Revision: 1760
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1752
-Last Changed Date: 2014-08-29 12:17:54 -0400 (Fri, 29 Aug 2014)
+Last Changed Rev: 1760
+Last Changed Date: 2014-09-02 14:53:34 -0400 (Tue, 02 Sep 2014)
 
 '''
