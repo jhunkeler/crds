74cbcd71914beab4ea946281122361fad76d77f6
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-08 19:15:37 +0000

    Merged fix for hst.locate.properties_inside_mapping onto milestone-7-p
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/milestone-7-p@1782 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1778M'
+__svn_version__ = '1780M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/milestone-7-p
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1778
+Revision: 1780
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1777
-Last Changed Date: 2014-09-05 18:11:24 -0400 (Fri, 05 Sep 2014)
+Last Changed Rev: 1780
+Last Changed Date: 2014-09-06 11:56:53 -0400 (Sat, 06 Sep 2014)
 
 '''
