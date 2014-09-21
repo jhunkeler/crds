dafcf0b1cf0059ac4b1e48722d0ded39a41cf025
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-21 18:12:51 +0000

    Added api.get_context_history.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.3-server@1825 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1780M'
+__svn_version__ = '1824M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/milestone-7-p
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.3-server
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1780
+Revision: 1824
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1780
-Last Changed Date: 2014-09-06 11:56:53 -0400 (Sat, 06 Sep 2014)
+Last Changed Rev: 1815
+Last Changed Date: 2014-09-16 11:03:32 -0400 (Tue, 16 Sep 2014)
 
 '''
