314677c9c82caa8246732a0d974b816988a9f3dd
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-10-24 19:39:44 +0000

    Merged affected datasets optimizations and table effects for 3 new COS types onto opus_2014.4-server branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4-server@1949 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '1926M'
+__svn_version__ = '1947M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4-server
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 1926
+Revision: 1947
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 1925
-Last Changed Date: 2014-10-16 13:42:44 -0400 (Thu, 16 Oct 2014)
+Last Changed Rev: 1929
+Last Changed Date: 2014-10-16 15:45:21 -0400 (Thu, 16 Oct 2014)
 
 '''
