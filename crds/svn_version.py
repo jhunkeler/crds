82cfdeeef727531fda21208760e947ee113fe5ef
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-01-16 20:59:29 +0000

    Fix for CrdsError __call__ vs. __init__ temporary insanity, " " joined exception parameters.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2@2084 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2081M'
+__svn_version__ = '2082:2083M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2081
+Revision: 2083
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2016
-Last Changed Date: 2014-12-10 17:59:48 -0500 (Wed, 10 Dec 2014)
+Last Changed Rev: 2083
+Last Changed Date: 2015-01-16 13:34:55 -0500 (Fri, 16 Jan 2015)
 
 '''
