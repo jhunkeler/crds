f30eb71b695c1d90e81ecda8aa2efb2c8b3a3117
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-01-20 15:37:47 +0000

    Commited svn_version on opus_2014.4a-server-2
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2@2088 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,15 @@
-__svn_version__ = '2079:2080M'
+__svn_version__ = '2087M'
 
 __full_svn_info__ = '''
 Path: .
-Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2079
+Revision: 2087
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2079
-Last Changed Date: 2015-01-08 16:13:04 -0500 (Thu, 08 Jan 2015)
+Last Changed Rev: 2087
+Last Changed Date: 2015-01-16 16:54:14 -0500 (Fri, 16 Jan 2015)
 
 '''
