f7795a169cd8ad09669bf7d27a882635a650bc45
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-02-04 20:38:05 +0000

    Fixed bug in rmap parkey set arithmetic for bestrefs explorer.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2@2123 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2087M'
+__svn_version__ = '2122M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2087
+Revision: 2122
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2087
-Last Changed Date: 2015-01-16 16:54:14 -0500 (Fri, 16 Jan 2015)
+Last Changed Rev: 2088
+Last Changed Date: 2015-01-20 10:37:47 -0500 (Tue, 20 Jan 2015)
 
 '''
