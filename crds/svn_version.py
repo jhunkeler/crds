46a44caef08b55db76be8473bba7d2eaa40ef179
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-03-26 21:07:43 +0000

    SVN version on irafx branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/irafx@2197 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = ''
+__svn_version__ = '2196M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/irafx
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2188
+Revision: 2196
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2188
-Last Changed Date: 2015-03-23 14:37:13 -0400 (Mon, 23 Mar 2015)
+Last Changed Rev: 2196
+Last Changed Date: 2015-03-26 10:18:59 -0400 (Thu, 26 Mar 2015)
 
 '''
