58cd48d6136084b8b38fc3e6de98ad7dc32c8ca9
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-05-13 19:58:43 +0000

    Switched name of prototype COS FLATFILE changes from 251 to 250 recreating the download/modify/upload flow which generates a correct rmap derivation chain.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-3@2282 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2122M'
+__svn_version__ = '2278M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-3
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2122
+Revision: 2278
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2088
-Last Changed Date: 2015-01-20 10:37:47 -0500 (Tue, 20 Jan 2015)
+Last Changed Rev: 2278
+Last Changed Date: 2015-05-13 12:14:20 -0400 (Wed, 13 May 2015)
 
 '''
