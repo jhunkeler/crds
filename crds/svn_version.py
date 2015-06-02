c3d61099c4eb0f5eb03c283b8d5014daf43a1b29
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-02 12:57:42 +0000

    Updated branch revision and URL for opus_2015.2
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2@2347 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,16 +1,16 @@
-__svn_version__ = '2344M'
+__svn_version__ = '2346M'
 
 __full_svn_info__ = '''
 Path: .
 Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2344
+Revision: 2346
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2344
-Last Changed Date: 2015-05-29 11:00:41 -0400 (Fri, 29 May 2015)
+Last Changed Rev: 2346
+Last Changed Date: 2015-06-02 08:52:03 -0400 (Tue, 02 Jun 2015)
 
 '''
