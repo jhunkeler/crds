7497379f9db4c192646c14f83d29fda876aebc07
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-06-17 20:56:12 +0000

    Rolled back accidental commit on OPUS_2015.2 branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2@2394 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,16 @@
-__svn_version__ = '2390M'
+__svn_version__ = '2346M'
 
 __full_svn_info__ = '''
 Path: .
+Working Copy Root Path: /Users/jmiller/work/workspace_crds/CRDS
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2390
+Revision: 2346
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2347
-Last Changed Date: 2015-06-02 08:57:42 -0400 (Tue, 02 Jun 2015)
+Last Changed Rev: 2346
+Last Changed Date: 2015-06-02 08:52:03 -0400 (Tue, 02 Jun 2015)
 
 '''
