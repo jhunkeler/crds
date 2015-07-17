ac6c7288679025a54f267ba444fc637fcc0a7c65
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-07-17 17:00:56 +0000

    Captured svn version on opus_2015.2-server
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-server@2484 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2481M'
+__svn_version__ = '2481:2482M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-server
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2481
+Revision: 2482
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2479
-Last Changed Date: 2015-07-16 07:52:54 -0400 (Thu, 16 Jul 2015)
+Last Changed Rev: 2482
+Last Changed Date: 2015-07-17 11:16:29 -0400 (Fri, 17 Jul 2015)
 
 '''
