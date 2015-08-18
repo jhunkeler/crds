7274325b726dd411725eb9940aa7b96b81e6ad0d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-18 19:28:35 +0000

    Merged ACS HRC CCDGAIN=-999 expansion from trunk to opus_2015.2-client-2
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2@2538 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2514M'
+__svn_version__ = '2535M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2514
+Revision: 2535
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2514
-Last Changed Date: 2015-08-05 17:03:10 -0400 (Wed, 05 Aug 2015)
+Last Changed Rev: 2516
+Last Changed Date: 2015-08-06 11:28:50 -0400 (Thu, 06 Aug 2015)
 
 '''
