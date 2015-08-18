42f3f85fbee4706fb6d0d33a205321ff113147a6
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-18 17:55:24 +0000

    Merge expansion rule for ACS HRC CCDGAIN=-999 from trunk.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2@2537 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2122M'
+__svn_version__ = '2535M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2014.4a-server-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2122
+Revision: 2535
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2088
-Last Changed Date: 2015-01-20 10:37:47 -0500 (Tue, 20 Jan 2015)
+Last Changed Rev: 2123
+Last Changed Date: 2015-02-04 15:38:05 -0500 (Wed, 04 Feb 2015)
 
 '''
