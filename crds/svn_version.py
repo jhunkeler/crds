642ab75e3acbc7ab39ff6d9f4ba8318caad61871
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-10-20 18:31:09 +0000

    Updated opus_2015.2 for COS SPOTTAB.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2@2661 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2535M'
+__svn_version__ = '2659M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2
+URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2535
+Revision: 2659
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2516
-Last Changed Date: 2015-08-06 11:28:50 -0400 (Thu, 06 Aug 2015)
+Last Changed Rev: 2659
+Last Changed Date: 2015-10-19 16:15:51 -0400 (Mon, 19 Oct 2015)
 
 '''
