9309fd2dcb1a7fb3e186d22127ef64981bc71c64
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-10-28 16:25:40 +0000

    Fixed COS SPOTTAB COMMENT .tpn _ld.tpn errors. Renamed WFC3 DRC .tpns to DKC tpns.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2@2682 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2659M'
+__svn_version__ = '2681M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2659
+Revision: 2681
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2659
-Last Changed Date: 2015-10-19 16:15:51 -0400 (Mon, 19 Oct 2015)
+Last Changed Rev: 2661
+Last Changed Date: 2015-10-20 14:31:09 -0400 (Tue, 20 Oct 2015)
 
 '''
