81cca0f2064dc831c56bb899ba897bbbefebe6d2
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-17 22:38:31 +0000

    Added UVIS2-C1K1C-CTE,UVIS2-C512C-CTE to WFC3 APERTURE for opus-2015.2-client branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2@2738 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2681M'
+__svn_version__ = '2736M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2681
+Revision: 2736
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2661
-Last Changed Date: 2015-10-20 14:31:09 -0400 (Tue, 20 Oct 2015)
+Last Changed Rev: 2682
+Last Changed Date: 2015-10-28 12:25:40 -0400 (Wed, 28 Oct 2015)
 
 '''
