7aef712826b247a4f59aa551709545aa82066049
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-11-18 16:06:04 +0000

    Merged WFC3 BIASFILE support for APERTURE UVIS2-C1K1C-CTE,UVIS2-C512C-CTE to opus-2015.2-client-2 branch.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2@2740 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2736M'
+__svn_version__ = '2738M'
 
 __full_svn_info__ = '''
 Path: .
 URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.2-client-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2736
+Revision: 2738
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2682
-Last Changed Date: 2015-10-28 12:25:40 -0400 (Wed, 28 Oct 2015)
+Last Changed Rev: 2738
+Last Changed Date: 2015-11-17 17:38:31 -0500 (Tue, 17 Nov 2015)
 
 '''
