03ebb999b3c979544ce91d95ca71dbc15a8fbe62
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-12-23 03:15:41 +0000

    Saved branch svn info.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.3-server-2@2776 0d8f46c8-9b30-49d0-b470-0f6283dc92e8

--- crds/svn_version.py
+++ crds/svn_version.py
@@ -1,15 +1,15 @@
-__svn_version__ = '2772M'
+__svn_version__ = '2773:2775M'
 
 __full_svn_info__ = '''
 Path: .
-URL: https://aeon.stsci.edu/ssb/svn/crds/trunk
+URL: https://aeon.stsci.edu/ssb/svn/crds/branches/opus_2015.3-server-2
 Repository Root: https://aeon.stsci.edu/ssb/svn/crds
 Repository UUID: 0d8f46c8-9b30-49d0-b470-0f6283dc92e8
-Revision: 2772
+Revision: 2775
 Node Kind: directory
 Schedule: normal
 Last Changed Author: jmiller@stsci.edu
-Last Changed Rev: 2772
-Last Changed Date: 2015-12-21 13:46:19 -0500 (Mon, 21 Dec 2015)
+Last Changed Rev: 2775
+Last Changed Date: 2015-12-22 21:34:01 -0500 (Tue, 22 Dec 2015)
 
 '''
